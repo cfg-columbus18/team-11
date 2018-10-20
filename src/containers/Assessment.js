4ad0362f36c6bbf22407344240ce1df0e6e65b88
Anna Hattle <annahattle@Annas-MacBook-Pro-5.local>
2018-10-20 03:33:58 -0400

    emoji fixes

--- src/containers/Assessment.js
+++ src/containers/Assessment.js
@@ -27,11 +27,22 @@ export default class Assessment extends Component {
 
                 <div className="panel" onClick={this.handleFinishAssessment("happy")}>
 
-                    <Image src="https://cdn.psychologytoday.com/sites/default/files/blogs/38/2008/12/2598-75772.jpg" responsive/>
+                    {/*happy*/}
+                    <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu-OITf7IzUdFWRGccUxOuOaONEBI8L2e9_aJ_nsiOG0DzhlQm" responsive/>
+                    {/*safe*/}
+                    <Image src="https://png2.kisspng.com/show/a706614c684acb61222c766d300a6d1b/L0KzQYm3VcA6N5x5fZH0aYP2gLBuTfVud5tuiNdtaXGwd7b6lQVzbV5tedDtc3jke7a0VfFnO2o3SqdrMEW3dIS1VcI6OWc7Uao6NUK4SYK5UcAyP2M3S5D5bne=/kisspng-emojipedia-gesture-handshake-5af39225b054d3.5291669815259121017223.png" responsive/>
+                    {/*alright*/}
+                    <Image src="https://cdn.shopify.com/s/files/1/1061/1924/products/Slightly_Smiling_Emoji_Icon_34f238ed-d557-4161-b966-779d8f37b1ac_large.png?v=1513251058" responsive/>
+                    {/*angry*/}
                     <Image src="https://cdn.shopify.com/s/files/1/1061/1924/products/Angry_Face_Emoji_Icon_ios10_large.png?v=1513251030" responsive/>
+                    {/*depressed*/}
                     <Image src="https://cdn.shopify.com/s/files/1/1061/1924/files/Sad_Face_Emoji.png?9898922749706957214" responsive/>
+                    {/*anxious*/}
+                    <Image src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/114/disappointed-but-relieved-face_1f625.png" responsive/>
+                    {/*stressed*/}
                     <Image src="https://mbtskoudsalg.com/images/confused-emojis-png-6.png" responsive/>
-                    <Image src="https://sites.google.com/site/quilliamsart/_/rsrc/1457630700087/portfolio/digital-art/anxious-emoji/Emoji%20Animation%20%28Shawn%20Quilliams%29.gif" responsive/>
+                    {/*unsafe*/}
+                    <Image src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/76/face-screaming-in-fear_1f631.png" responsive/>
                 </div>
             </div>
         );
